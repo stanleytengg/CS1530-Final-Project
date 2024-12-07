8f8d0c1aad155812572ebdbd28c610adc0ae97db
Stanley Teng <stanley97043@gmail.com>
2024-12-07 11:44:31 -0500

    Add Expense section in database

--- models.py
+++ models.py
@@ -7,6 +7,14 @@ class User(db.Model, UserMixin):
     uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
+    expenses = db.relationship('Expense', backref='user', lazy=True)
     
     def get_id(self):
         return self.uid
+
+class Expense(db.Model):
+    __tablename__ = 'expenses'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.uid'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
